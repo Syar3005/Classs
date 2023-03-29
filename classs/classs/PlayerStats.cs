230345be4dae72693a842024d1eebc3024328050
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2023-03-29 20:29:29 +0300

    Add files via upload

---
+++ classs/classs/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classs
+{
+    public class PlayerStats
+    {
+        public int Kills { get; set; }
+        public int Assists { get; set; }
+        public int Deaths { get; set; }
+
+        public PlayerStats() { }
+        public PlayerStats(int kills, int assists, int deaths)
+        {
+            Kills = kills;
+            Assists = assists;
+            Deaths = deaths;
+        }
+
+        public override string ToString()
+        {
+            return $"Kills: {Kills} Assists: {Assists} Deaths: {Deaths}";
+        }
+    }
+}
---
+++ classs/classs/Program.cs
@@ -0,0 +1,14 @@
+﻿using classs;
+
+PlayerStats[] statsArr = new PlayerStats[5];
+
+statsArr[0] = new PlayerStats(10, 3, 7);
+statsArr[1] = new PlayerStats(8, 2, 9);
+statsArr[2] = new PlayerStats(8, 0, 8);
+statsArr[3] = new PlayerStats(6, 4, 10);
+statsArr[4] = new PlayerStats(3, 6, 11);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(statsArr[i].ToString());
+}
